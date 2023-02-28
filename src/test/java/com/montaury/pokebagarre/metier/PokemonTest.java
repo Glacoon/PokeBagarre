0942499d283739c58eddd919e9fe381c86ddbf7f
CELLE Guillian <guillian.celle@gmail.com>
2023-02-28 11:11:59 +0100

    TP2

--- src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/PokemonTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PokemonTest {
-    Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(100).avecDefense(100).construire();
+   /* Pokemon pokemon1 = new ConstructeurDePokemon().avecAttaque(100).avecDefense(100).construire();
     @Test
     void premier_pokemon_gagne_avec_attaque_superieur() {
         //GIVEN
@@ -62,6 +62,65 @@ class PokemonTest {
 
         //THEN
         assertEquals(true, vainqueur);
+    }*/
+
+    Pokemon pokemonTest = new ConstructeurDePokemon().avecAttaque(100).avecDefense(100).construire();
+
+    @Test
+    void premier_pkmn_gagne_avec_attaque_sup(){
+        //GIVEN
+        Pokemon pokemon = new ConstructeurDePokemon().avecAttaque(50).avecDefense(100).construire();
+
+        //WHEN
+        Boolean vainqueur = pokemonTest.estVainqueurContre(pokemon);
+
+        //THEN
+        assertEquals(true, vainqueur);
     }
 
+    @Test
+    void premier_pkmn_perd_avec_attaque_inf(){
+        //GIVEN
+        Pokemon pokemon = new ConstructeurDePokemon().avecAttaque(150).avecDefense(100).construire();
+
+        //WHEN
+        Boolean vainqueur = pokemonTest.estVainqueurContre(pokemon);
+
+        //THEN
+        assertEquals(false, vainqueur);
+    }
+
+    @Test
+    void premier_pkmn_gagne_avec_defense_supp(){
+        Pokemon pokemon = new ConstructeurDePokemon().avecAttaque(100).avecDefense(10).construire();
+
+        //WHEN
+        Boolean vainqueur = pokemonTest.estVainqueurContre(pokemon);
+
+        //THEN
+        assertEquals(true, vainqueur);
+    }
+
+    @Test
+    void premier_pkmn_perd_avec_defense_inf(){
+        Pokemon pokemon = new ConstructeurDePokemon().avecAttaque(100).avecDefense(150).construire();
+
+        //WHEN
+        Boolean vainqueur = pokemonTest.estVainqueurContre(pokemon);
+
+        //THEN
+        assertEquals(false, vainqueur);
+    }
+
+    @Test
+    void premier_pokemon_gagne_avec_attaque_et_defense_identiques() {
+        //GIVEN
+        Pokemon pokemon = new ConstructeurDePokemon().avecAttaque(100).avecDefense(100).construire();
+
+        //WHEN
+        Boolean vainqueur = pokemonTest.estVainqueurContre(pokemon);
+
+        //THEN
+        assertEquals(true, vainqueur);
+    }
 }
